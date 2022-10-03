6c288b08671905a7adbf0a74629a2d701be4b18f
clark94 <gomes.lucas@outlook.com.br>
2022-10-03 20:41:16 -0300

    Teste Commit

---
+++ abobora.go
@@ -0,0 +1,13 @@
+package abobora
+
+import "fmt"
+
+//Funcao Cebola nao faz nada
+func Cebola() {
+	fmt.Printf("Isto é uma cebola")
+}
+
+//Funcao teletubies
+func Teletubbies() {
+	fmt.Printf("Isto e uma teletubbies")
+}
